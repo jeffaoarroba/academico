59d46dbd9181463b47c54a77abb3a49c9061d38d
Jefferson Andrade <jeffaoarroba@gmail.com>
2025-06-30 18:57:52 -0300

    feat(servico/viacep): buscar endereco completo com o cep informado

---
+++ src/servico/via_cep.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+
+class ViaCEP:
+    def __init__(self, cep):
+        self.cep = cep
+
+    def obter_endereco_completo(self):
+        """
+            https://viacep.com.br/ws/CEP/json/
+            Faz uma requisição HTTP para a API ViaCEP, busca o endereço
+            e retorna uma string formatada com o endereço completo.
+        """
+
+        if len(self.cep) != 8:
+            return {"erro": "O CEP informado não é válido!"}
+
+        url = f"https://viacep.com.br/ws/{self.cep}/json/"
+
+        try:
+            # faz uma requisicao http, solicitando as informacoes do CEP, para o servico viacep
+            response = requests.get(url)
+            # gera um erro se o status da requisicao nao for OK (sucesso)
+            response.raise_for_status()
+            # le a resposta (da requisicao) com as informacoes do CEP
+            data = response.json()
+
+            if 'erro' in data and data['erro']:
+                print("ERRO SERVICO CEP", self.cep, "não encontrado.")
+                return {"erro": "CEP '" + self.cep + "' não encontrado."}
+
+            # informacoes do endereco
+            logradouro = data.get('logradouro', '')
+            bairro = data.get('bairro', '')
+            localidade = data.get('localidade', '')
+            uf = data.get('uf', '')
+
+            return {
+                "erro": False,
+                "endereco_completo": logradouro + ", " + bairro + ", " + localidade + "-" + uf + " CEP:" + self.cep
+            }
+        except Exception as e:
+            print("ERRO SERVICO CEP Ocorreu um erro inesperado", e)
+            return {"erro": "Ocorreu um erro inesperado"}
+
+
+if __name__ == "__main__":
+    endereco_completo = ViaCEP("29171-31").obter_endereco_completo()
+    print("endereco:", endereco_completo)
